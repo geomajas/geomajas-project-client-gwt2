3ddd3cf494e697306387a510d45529811c79fcbc
dosi-bingov <dosi.bingov@dfc.be>
2014-01-17 11:21:22 +0100

    PURE-217 Migrate to standard GWT logging - browser console logging enabled in example  project

--- impl/src/main/java/org/geomajas/gwt2/client/controller/TouchNavigationController.java
+++ impl/src/main/java/org/geomajas/gwt2/client/controller/TouchNavigationController.java
@@ -10,10 +10,6 @@
  */
 package org.geomajas.gwt2.client.controller;
 
-import org.geomajas.geometry.Coordinate;
-import org.geomajas.gwt.client.map.RenderSpace;
-import org.geomajas.gwt2.client.map.MapPresenter;
-
 import com.google.gwt.event.dom.client.GestureChangeEvent;
 import com.google.gwt.event.dom.client.GestureEndEvent;
 import com.google.gwt.event.dom.client.GestureStartEvent;
@@ -22,15 +18,23 @@ import com.google.gwt.event.dom.client.TouchEndEvent;
 import com.google.gwt.event.dom.client.TouchEvent;
 import com.google.gwt.event.dom.client.TouchMoveEvent;
 import com.google.gwt.event.dom.client.TouchStartEvent;
+import org.geomajas.geometry.Coordinate;
+import org.geomajas.gwt.client.map.RenderSpace;
+import org.geomajas.gwt2.client.map.MapPresenter;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Generic controller that is used on touch mobile devices. Note that gestures and multi touch are not supported by some
  * mobile browsers.
- * 
+ *
  * @author Dosi Bingov
  * @since 1.0.0
  */
 public class TouchNavigationController extends AbstractMapController {
+
+	private static Logger logger = Logger.getLogger("");
 
 	protected Coordinate touchedOrigin;
 
@@ -62,15 +66,16 @@ public class TouchNavigationController extends AbstractMapController {
 
 	@Override
 	public void onTouchEnd(TouchEndEvent event) {
-		updateView(event, true);
+		updateView(event);
 	}
 
 	@Override
 	public void onTouchMove(TouchMoveEvent event) {
+		logger.log(Level.INFO, "TouchNavigationController -> onTouchMove()");
 		event.preventDefault();
 		midPoint = mapPresenter.getViewPort().getTransformationService()
-				.transform(getMidPoint(event), RenderSpace.SCREEN, RenderSpace.WORLD);
-		updateView(event, true); // TODO second argument should be false but onTouchEnd is not fired
+		  .transform(getMidPoint(event), RenderSpace.SCREEN, RenderSpace.WORLD);
+		updateView(event);
 	}
 
 	@Override
@@ -83,6 +88,7 @@ public class TouchNavigationController extends AbstractMapController {
 
 	@Override
 	public void onGestureStart(GestureStartEvent event) {
+		logger.log(Level.INFO, "TouchNavigationController -> onGestureStart()");
 		event.preventDefault();
 		lastScale = mapPresenter.getViewPort().getScale();
 		zoomTo(event.getScale(), false);
@@ -90,12 +96,14 @@ public class TouchNavigationController extends AbstractMapController {
 
 	@Override
 	public void onGestureEnd(GestureEndEvent event) {
+		logger.log(Level.INFO, "TouchNavigationController -> onGestureEnd()");
 		event.preventDefault();
 		zoomTo(event.getScale(), true);
 	}
 
 	@Override
 	public void onGestureChange(GestureChangeEvent event) {
+		logger.log(Level.INFO, "TouchNavigationController -> onGestureChange()");
 		event.preventDefault();
 		zoomTo(event.getScale(), false);
 	}
@@ -115,18 +123,19 @@ public class TouchNavigationController extends AbstractMapController {
 	}
 
 	protected void zoomTo(double scale, boolean isGestureEnded) {
-//		if (midPoint != null) {
-//			mapPresenter.getViewPort().applyScale(scale * lastScale, midPoint);
-//		} else {
-			mapPresenter.getViewPort().applyScale(scale * lastScale);
-//		}
+		logger.log(Level.INFO,
+		  "TouchNavigationController -> zoomTo(scale=" + scale + ",isGestureEnded " + isGestureEnded + " )");
+		//		if (midPoint != null) {
+		//			mapPresenter.getViewPort().applyScale(scale * lastScale, midPoint);
+		//		} else {
+		mapPresenter.getViewPort().applyScale(scale * lastScale);
+		//		}
 	}
 
 	/**
 	 * Method used to calculate exact middle point between multiple touches of a touch events.
-	 * 
-	 * @param event
-	 *            a touch event
+	 *
+	 * @param event a touch event
 	 * @return middle point
 	 */
 	protected Coordinate getMidPoint(TouchEvent<?> event) {
@@ -151,16 +160,17 @@ public class TouchNavigationController extends AbstractMapController {
 
 	/**
 	 * Update the view of the map when touching and dragging.
-	 * 
+	 *
 	 * @param event
 	 */
-	protected void updateView(TouchEvent<?> event, boolean isTouchEnded) {
+	protected void updateView(TouchEvent<?> event) {
+		logger.log(Level.INFO, "TouchNavigationController -> updateView(TouchEvent<?> event, boolean isTouchEnded)");
 		Coordinate end = getMidPoint(event);
 
 		Coordinate beginWorld = mapPresenter.getViewPort().getTransformationService()
-				.transform(touchedOrigin, RenderSpace.SCREEN, RenderSpace.WORLD);
+		  .transform(touchedOrigin, RenderSpace.SCREEN, RenderSpace.WORLD);
 		Coordinate endWorld = mapPresenter.getViewPort().getTransformationService()
-				.transform(end, RenderSpace.SCREEN, RenderSpace.WORLD);
+		  .transform(end, RenderSpace.SCREEN, RenderSpace.WORLD);
 		double x = mapPresenter.getViewPort().getPosition().getX() + beginWorld.getX() - endWorld.getX();
 		double y = mapPresenter.getViewPort().getPosition().getY() + beginWorld.getY() - endWorld.getY();
 
--- impl/src/main/java/org/geomajas/gwt2/client/widget/control/zoom/ZoomControl.java
+++ impl/src/main/java/org/geomajas/gwt2/client/widget/control/zoom/ZoomControl.java
@@ -31,6 +31,9 @@ import org.geomajas.gwt2.client.map.MapPresenter;
 import org.geomajas.gwt2.client.map.ViewPort;
 import org.geomajas.gwt2.client.widget.AbstractMapWidget;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Map widget that displays zoom in, zoom out and zoom to maximum extent buttons. This widget is meant to be added to
  * the map's widget pane (see {@link MapPresenter#getWidgetPane()}).
@@ -39,6 +42,7 @@ import org.geomajas.gwt2.client.widget.AbstractMapWidget;
  * @author Dosi Bingov
  */
 public class ZoomControl extends AbstractMapWidget {
+	private static Logger logger = Logger.getLogger("");
 
 	/**
 	 * UI binder definition for the {@link ZoomControl} widget.
@@ -108,6 +112,7 @@ public class ZoomControl extends AbstractMapWidget {
 	 * Initialize handlers for desktop browser.
 	 */
 	private void initializeForDesktop() {
+		logger.log(Level.INFO, "ZoomControl ->initializeForDesktop()");
 		StopPropagationHandler preventWeirdBehaviourHandler = new StopPropagationHandler();
 		addDomHandler(preventWeirdBehaviourHandler, MouseDownEvent.getType());
 		addDomHandler(preventWeirdBehaviourHandler, MouseUpEvent.getType());
@@ -120,6 +125,7 @@ public class ZoomControl extends AbstractMapWidget {
 		zoomInElement.addDomHandler(new ClickHandler() {
 
 			public void onClick(ClickEvent event) {
+				logger.log(Level.INFO, "ZoomControl -> zoomInElement onClick()");
 				int index = viewPort.getFixedScaleIndex(viewPort.getScale());
 				if (index < viewPort.getFixedScaleCount() - 1) {
 					viewPort.registerAnimation(NavigationAnimationFactory.createZoomIn(mapPresenter));
@@ -132,6 +138,7 @@ public class ZoomControl extends AbstractMapWidget {
 		zoomOutElement.addDomHandler(new ClickHandler() {
 
 			public void onClick(ClickEvent event) {
+				logger.log(Level.INFO, "ZoomControl -> zoomOutElement onClick()");
 				int index = viewPort.getFixedScaleIndex(viewPort.getScale());
 				if (index > 0) {
 					viewPort.registerAnimation(NavigationAnimationFactory.createZoomOut(mapPresenter));
@@ -145,11 +152,13 @@ public class ZoomControl extends AbstractMapWidget {
 	 * Initialize handlers for mobile devices.
 	 */
 	private void initializeForTouchDevice() {
+		logger.log(Level.INFO, "ZoomControl -> initializeForTouchDevice()");
 		// Add touch handlers to the zoom in button:
 		zoomInElement.addDomHandler(new TouchStartHandler() {
 
 			@Override
 			public void onTouchStart(TouchStartEvent event) {
+				logger.log(Level.INFO, "ZoomControl -> zoomInElement onTouchStart()");
 				ViewPort viewPort = mapPresenter.getViewPort();
 				int index = viewPort.getFixedScaleIndex(viewPort.getScale());
 
@@ -166,6 +175,7 @@ public class ZoomControl extends AbstractMapWidget {
 
 			@Override
 			public void onTouchStart(TouchStartEvent event) {
+				logger.log(Level.INFO, "zoomOutElement -> zoomInElement onTouchStart()");
 				ViewPort viewPort = mapPresenter.getViewPort();
 				int index = viewPort.getFixedScaleIndex(viewPort.getScale());
 
