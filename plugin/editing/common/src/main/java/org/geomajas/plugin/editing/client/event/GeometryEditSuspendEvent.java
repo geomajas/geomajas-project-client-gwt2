5340f573c01f8d8806fbdd0886cd4eda4519df97
Jan De Moerloose <jan.demoerloose@geosparc.com>
2014-03-10 17:46:15 +0100

    GWTII-123: suspend()/resume() for GeometryEditService

---
+++ plugin/editing/common/src/main/java/org/geomajas/plugin/editing/client/event/GeometryEditSuspendEvent.java
@@ -0,0 +1,51 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2013 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.plugin.editing.client.event;
+
+import org.geomajas.geometry.Geometry;
+import org.geomajas.annotation.FutureApi;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+/**
+ * Event that reports the editing of a geometry has been suspended.
+ * 
+ * @author Jan De Moerloose
+ * @since 2.1.0
+ */
+@FutureApi(allMethods = true)
+public class GeometryEditSuspendEvent extends GwtEvent<GeometryEditSuspendHandler> {
+
+	private final Geometry geometry;
+
+	public GeometryEditSuspendEvent(Geometry geometry) {
+		this.geometry = geometry;
+	}
+
+	@Override
+	public Type<GeometryEditSuspendHandler> getAssociatedType() {
+		return GeometryEditSuspendHandler.TYPE;
+	}
+
+	@Override
+	protected void dispatch(GeometryEditSuspendHandler geometryEditWorkflowHandler) {
+		geometryEditWorkflowHandler.onGeometryEditSuspend(this);
+	}
+
+	/**
+	 * Get the geometry that will be edited.
+	 * @return The geometry that is to be edited.
+	 */
+	public Geometry getGeometry() {
+		return geometry;
+	}
+}
--- plugin/editing/common/src/main/java/org/geomajas/plugin/editing/client/service/GeometryEditService.java
+++ plugin/editing/common/src/main/java/org/geomajas/plugin/editing/client/service/GeometryEditService.java
@@ -18,9 +18,11 @@ import org.geomajas.plugin.editing.client.event.GeometryEditChangeStateHandler;
 import org.geomajas.plugin.editing.client.event.GeometryEditInsertHandler;
 import org.geomajas.plugin.editing.client.event.GeometryEditMoveHandler;
 import org.geomajas.plugin.editing.client.event.GeometryEditRemoveHandler;
+import org.geomajas.plugin.editing.client.event.GeometryEditResumeHandler;
 import org.geomajas.plugin.editing.client.event.GeometryEditShapeChangedHandler;
 import org.geomajas.plugin.editing.client.event.GeometryEditStartHandler;
 import org.geomajas.plugin.editing.client.event.GeometryEditStopHandler;
+import org.geomajas.plugin.editing.client.event.GeometryEditSuspendHandler;
 import org.geomajas.plugin.editing.client.event.GeometryEditTentativeMoveHandler;
 
 import com.google.gwt.event.shared.HandlerRegistration;
@@ -73,6 +75,24 @@ public interface GeometryEditService extends GeometryIndexOperationService {
 	 * @return The registration of the handler.
 	 */
 	HandlerRegistration addGeometryEditStopHandler(GeometryEditStopHandler handler);
+
+	/**
+	 * Register a {@link GeometryEditSuspendHandler} to listen to events that signal the editing process has been suspended.
+	 * 
+	 * @param handler
+	 *            The {@link GeometryEditSuspendHandler} to add as listener.
+	 * @return The registration of the handler.
+	 */
+	HandlerRegistration addGeometryEditSuspendHandler(GeometryEditSuspendHandler handler);
+
+	/**
+	 * Register a {@link GeometryEditResumeHandler} to listen to events that signal the editing process has been resumed.
+	 * 
+	 * @param handler
+	 *            The {@link GeometryEditResumeHandler} to add as listener.
+	 * @return The registration of the handler.
+	 */
+	HandlerRegistration addGeometryEditResumeHandler(GeometryEditResumeHandler handler);
 
 	/**
 	 * Register a {@link GeometryEditShapeChangedHandler} to listen to operation events (moving, inserting, deleting,
@@ -154,6 +174,26 @@ public interface GeometryEditService extends GeometryIndexOperationService {
 	 * @return started state
 	 */
 	boolean isStarted();
+	
+	/**
+	 * Suspend the current editing state. Removes all editing controllers from the geometry and the map. This methods
+	 * allows to interrupt the editing session and temporarily activate a different map controller (e.g measure
+	 * distance). 
+	 * Set your map controller after calling this method and call {@link #resume()} when done.
+	 */
+	void suspend();
+
+	/**
+	 * Get the suspension state of the editing process.
+	 * 
+	 * @return suspension state
+	 */
+	boolean isSuspended();
+	
+	/**
+	 * Resumes the suspended editing state. Resets all editing controllers on the geometry and the map.
+	 */
+	void resume();
 
 	/**
 	 * Stop the geometry editing process.
