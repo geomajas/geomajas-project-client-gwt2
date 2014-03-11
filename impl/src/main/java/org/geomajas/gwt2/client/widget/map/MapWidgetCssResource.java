5093ba20b0f648a32c458e1e7ff1adfe3263b8f8
Pieter De Graef <pieter.degraef@geosparc.com>
2014-03-11 20:12:20 +0100

    GWTII-101: Updating API annotations to version 2.1.0

--- impl/src/main/java/org/geomajas/gwt2/client/widget/map/MapWidgetCssResource.java
+++ impl/src/main/java/org/geomajas/gwt2/client/widget/map/MapWidgetCssResource.java
@@ -18,7 +18,7 @@ import org.geomajas.annotation.Api;
  * CSS resource bundle for the {@link org.geomajas.gwt2.client.widget.control.pan.PanControl} widget.
  * 
  * @author Pieter De Graef
- * @since 2.0.0
+ * @since 2.1.0
  */
 @Api(allMethods = true)
 public interface MapWidgetCssResource extends CssResource {
