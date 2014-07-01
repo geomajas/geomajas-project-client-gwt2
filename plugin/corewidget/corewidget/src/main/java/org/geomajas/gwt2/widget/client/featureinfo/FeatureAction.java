6e0e677c68da8f1ea65c7c036988f80dbbe0c704
Youri Flement <youri.flement@ugent.be>
2014-07-01 15:12:23 +0200

    GWTII-164: Checkstyle and javadoc for FeatureActions.

--- plugin/corewidget/corewidget/src/main/java/org/geomajas/gwt2/widget/client/featureinfo/FeatureAction.java
+++ plugin/corewidget/corewidget/src/main/java/org/geomajas/gwt2/widget/client/featureinfo/FeatureAction.java
@@ -1,25 +1,36 @@
 /*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
  *
- *  * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
- *  *
- *  * Copyright 2008-2014 Geosparc nv, http://www.geosparc.com/, Belgium.
- *  *
- *  * The program is available in open source according to the GNU Affero
- *  * General Public License. All contributions in this program are covered
- *  * by the Geomajas Contributors License Agreement. For full licensing
- *  * details, see LICENSE.txt in the project root.
+ * Copyright 2008-2014 Geosparc nv, http://www.geosparc.com/, Belgium.
  *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
  */
 
 package org.geomajas.gwt2.widget.client.featureinfo;
 
+import org.geomajas.annotation.Api;
 import org.geomajas.gwt2.client.map.feature.Feature;
 
 /**
+ * Interface for a feature action. A feature action can be added to a {@link FeatureInfoWidget} to automatically
+ * synchronize the underlying {@link Feature} of the action to the selected {@link Feature}.
+ *
+ * Because the interface contains only a method to set the feature, a feature action could be a widget, logger, ...
+ *
  * @author Youri Flement
+ * @since 2.1.0
  */
+@Api(allMethods = true)
 public interface FeatureAction {
 
+	/**
+	 * Set the feature associated with the action.
+	 *
+	 * @param feature
+	 */
 	void setFeature(Feature feature);
 
 }
--- plugin/corewidget/example-jar/src/main/java/org/geomajas/gwt2/widget/example/client/sample/featureinfo/ZoomToObjectAction.java
+++ plugin/corewidget/example-jar/src/main/java/org/geomajas/gwt2/widget/example/client/sample/featureinfo/ZoomToObjectAction.java
@@ -1,14 +1,12 @@
 /*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
  *
- *  * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
- *  *
- *  * Copyright 2008-2014 Geosparc nv, http://www.geosparc.com/, Belgium.
- *  *
- *  * The program is available in open source according to the GNU Affero
- *  * General Public License. All contributions in this program are covered
- *  * by the Geomajas Contributors License Agreement. For full licensing
- *  * details, see LICENSE.txt in the project root.
+ * Copyright 2008-2014 Geosparc nv, http://www.geosparc.com/, Belgium.
  *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
  */
 
 package org.geomajas.gwt2.widget.example.client.sample.featureinfo;
@@ -24,7 +22,7 @@ import org.geomajas.gwt2.client.map.feature.Feature;
 import org.geomajas.gwt2.widget.client.featureinfo.FeatureAction;
 
 /**
- * Implementation of a {@link org.geomajas.gwt2.widget.client.featureinfo.FeatureAction} to zoom to a feature on the map.
+ * Implementation of a {@link FeatureAction} to zoom to a feature on the map.
  *
  * @author Youri Flement
  */
