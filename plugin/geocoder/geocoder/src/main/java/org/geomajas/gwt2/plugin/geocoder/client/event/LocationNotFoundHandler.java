b29067105a7cda6840279ff8bb54362a86a589f1
janv <jan.venstermans@dfc.be>
2014-07-16 13:17:59 +0200

    GWTII-181: refactor geocoder plugin: add gwt2 in package names

---
+++ plugin/geocoder/geocoder/src/main/java/org/geomajas/gwt2/plugin/geocoder/client/event/LocationNotFoundHandler.java
@@ -0,0 +1,38 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2014 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.gwt2.plugin.geocoder.client.event;
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import org.geomajas.annotation.Api;
+import org.geomajas.annotation.UserImplemented;
+
+/**
+ * Handler for {@link SelectAlternativeEvent}.
+ *
+ * @author Jan Venstermans
+ * @since 2.1.0
+ */
+@Api(allMethods = true)
+@UserImplemented
+public interface LocationNotFoundHandler extends EventHandler {
+
+	/** Event type. */
+	GwtEvent.Type<LocationNotFoundHandler> TYPE = new GwtEvent.Type<LocationNotFoundHandler>();
+
+	/**
+	 * Called when the geocoder returns no result.
+	 *
+	 * @param event event
+	 */
+	void onLocationNotFound(LocationNotFoundEvent event);
+}
---
+++ plugin/geocoder/geocoder/src/main/java/org/geomajas/gwt2/plugin/geocoder/client/nostyle/GeocoderWidgetResourceNoStyle.java
@@ -0,0 +1,25 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2014 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.gwt2.plugin.geocoder.client.nostyle;
+
+import org.geomajas.gwt2.plugin.geocoder.client.widget.GeocoderWidgetCssResource;
+import org.geomajas.gwt2.plugin.geocoder.client.widget.GeocoderWidgetResource;
+
+/**
+ *   No style version of {@link GeocoderWidgetResource}.
+ * @author Jan Venstermans
+ */
+public interface GeocoderWidgetResourceNoStyle extends GeocoderWidgetResource {
+
+	@Source("geomajas-geocoderGadget-nostyle.css")
+	GeocoderWidgetCssResource css();
+}
