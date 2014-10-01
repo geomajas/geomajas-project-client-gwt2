1a05174446bb64404f0b6ff90d1aca450f283235
Jan De Moerloose <jan.demoerloose@geosparc.com>
2014-10-01 16:05:43 +0200

    GWTII-223: refactored gwt test as a normal unit test + named layer fix for version 1.1.1

---
+++ plugin/wms/wms/src/test/java/org/geomajas/gwt2/plugin/wms/client/service/GetCapabilitiesParseTest.java
@@ -0,0 +1,168 @@
+/*
+ * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
+ *
+ * Copyright 2008-2014 Geosparc nv, http://www.geosparc.com/, Belgium.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.geomajas.gwt2.plugin.wms.client.service;
+
+import com.google.gwt.core.client.Callback;
+import com.google.gwt.core.client.GWT;
+import com.google.gwtmockito.GwtMockitoTestRunner;
+
+import org.geomajas.geometry.Bbox;
+import org.geomajas.gwt2.plugin.wms.client.capabilities.WmsGetCapabilitiesInfo;
+import org.geomajas.gwt2.plugin.wms.client.capabilities.WmsLayerInfo;
+import org.geomajas.gwt2.plugin.wms.client.capabilities.WmsLayerLegendUrlInfo;
+import org.geomajas.gwt2.plugin.wms.client.capabilities.WmsLayerMetadataUrlInfo;
+import org.geomajas.gwt2.plugin.wms.client.capabilities.WmsLayerStyleInfo;
+import org.geomajas.gwt2.plugin.wms.client.capabilities.WmsOnlineResourceInfo;
+import org.geomajas.gwt2.plugin.wms.client.service.WmsService.WmsVersion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import junit.framework.Assert;
+
+/**
+ * GWT test that fetches capabilities documents from a servlet and tries to parse them. We test WMS versions 1.1.1 and
+ * 1.3.0.
+ * 
+ * @author Pieter De Graef
+ */
+@RunWith(GwtMockitoTestRunner.class)
+public class GetCapabilitiesParseTest extends AbstractWmsServiceTest {
+
+	private static final String CAPA_FILE = "/org/geomajas/gwt2/plugin/wms/server/GetCapabilities";
+
+	@Test
+	public void testCapabilities111() throws Exception {
+		prepareResponse(CAPA_FILE+"111.xml");
+		wmsService.getCapabilities(GWT.getModuleBaseURL() + CAPA_FILE, WmsVersion.V1_1_1,
+				new Callback<WmsGetCapabilitiesInfo, String>() {
+
+					public void onSuccess(WmsGetCapabilitiesInfo result) {
+						Assert.assertNotNull(result);
+
+						List<WmsLayerInfo> layers = result.getLayers();
+						Assert.assertNotNull(layers);
+						Assert.assertEquals(2, layers.size());
+
+						checkLayer(1, layers.get(0), WmsVersion.V1_1_1);
+						checkLayer(2, layers.get(1), WmsVersion.V1_1_1);
+					}
+
+					public void onFailure(String reason) {
+						Assert.fail(reason);
+					}
+				});
+	}
+
+	@Test
+	public void testCapabilities130() throws Exception {
+		prepareResponse(CAPA_FILE+"130.xml");
+		wmsService.getCapabilities(GWT.getModuleBaseURL() + CAPA_FILE, WmsVersion.V1_3_0,
+				new Callback<WmsGetCapabilitiesInfo, String>() {
+
+					public void onSuccess(WmsGetCapabilitiesInfo result) {
+						Assert.assertNotNull(result);
+
+						List<WmsLayerInfo> layers = result.getLayers();
+						Assert.assertNotNull(layers);
+						Assert.assertEquals(2, layers.size());
+
+						checkLayer(1, layers.get(0), WmsVersion.V1_3_0);
+						checkLayer(2, layers.get(1), WmsVersion.V1_3_0);
+					}
+
+					public void onFailure(String reason) {
+						Assert.fail(reason);
+					}
+				});
+	}
+
+	// ------------------------------------------------------------------------
+	// Private methods for testing the parsed capabilities documents:
+	// ------------------------------------------------------------------------
+
+	private void checkLayer(int index, WmsLayerInfo layer, WmsVersion version) {
+		if (index == 1) {
+			Assert.assertFalse(layer.isQueryable());
+		} else {
+			Assert.assertTrue(layer.isQueryable());
+		}
+		Assert.assertEquals("layer" + index + "Name", layer.getName());
+		Assert.assertEquals("layer" + index + "Title", layer.getTitle());
+		Assert.assertEquals("layer" + index + "Abstract", layer.getAbstract());
+
+		List<String> keywords = layer.getKeywords();
+		Assert.assertNotNull(keywords);
+		Assert.assertEquals(2, keywords.size());
+		Assert.assertEquals("keyword1", keywords.get(0));
+		Assert.assertEquals("keyword2", keywords.get(1));
+
+		List<String> crs = layer.getCrs();
+		Assert.assertNotNull(crs);
+		if (version.equals(WmsVersion.V1_1_1)) {
+			Assert.assertEquals(1, crs.size());
+			Assert.assertEquals("EPSG:31370", crs.get(0));
+		} else {
+			Assert.assertEquals(2, crs.size());
+			Assert.assertEquals("EPSG:31370", crs.get(0));
+			Assert.assertEquals("EPSG:4326", crs.get(1));
+		}
+
+		Bbox latlonBox = layer.getLatlonBoundingBox();
+		Assert.assertEquals(0.0, latlonBox.getX());
+		Assert.assertEquals(1.0, latlonBox.getMaxX());
+		Assert.assertEquals(2.0, latlonBox.getY());
+		Assert.assertEquals(3.0, latlonBox.getMaxY());
+
+		Assert.assertEquals("EPSG:31370", layer.getBoundingBoxCrs());
+		Bbox bounds = layer.getBoundingBox();
+		Assert.assertEquals(0.0, bounds.getX());
+		Assert.assertEquals(200000.0, bounds.getMaxX());
+		Assert.assertEquals(0.0, bounds.getY());
+		Assert.assertEquals(200000.0, bounds.getMaxY());
+
+		checkMetadataUrl(layer.getMetadataUrls());
+		List<WmsLayerStyleInfo> styles = layer.getStyleInfo();
+		Assert.assertNotNull(styles);
+		Assert.assertEquals(1, styles.size());
+		checkLayerStyle(index, styles.get(0));
+	}
+
+	private void checkMetadataUrl(List<WmsLayerMetadataUrlInfo> info) {
+		Assert.assertNotNull(info);
+		Assert.assertTrue(info.size() > 0);
+		Assert.assertEquals("ISO19115:2003", info.get(0).getType());
+		Assert.assertEquals("application/xml", info.get(0).getFormat());
+		checkOnlineResource(info.get(0).getOnlineResource());
+	}
+
+	private void checkOnlineResource(WmsOnlineResourceInfo info) {
+		Assert.assertEquals("http://www.geomajas.org/", info.getHref());
+		Assert.assertEquals("simple", info.getType());
+		Assert.assertEquals("xlink", info.getXLink());
+	}
+
+	private void checkLayerStyle(int index, WmsLayerStyleInfo info) {
+		Assert.assertEquals("layer" + index + "StyleName", info.getName());
+		Assert.assertEquals("layer" + index + "StyleTitle", info.getTitle());
+		Assert.assertEquals("layer" + index + "StyleAbstract", info.getAbstract());
+		checkLayerLegendUrl(info.getLegendUrl());
+	}
+
+	private void checkLayerLegendUrl(WmsLayerLegendUrlInfo info) {
+		Assert.assertEquals("image/png", info.getFormat());
+		Assert.assertEquals(20, info.getWidth());
+		Assert.assertEquals(20, info.getHeight());
+		checkOnlineResource(info.getOnlineResource());
+	}
+}
--- plugin/wms/wms/src/test/java/org/geomajas/gwt2/plugin/wms/client/service/WmsServiceTest.java
+++ plugin/wms/wms/src/test/java/org/geomajas/gwt2/plugin/wms/client/service/WmsServiceTest.java
@@ -55,7 +55,9 @@ import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import java.io.IOException;
 import java.io.StringReader;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -76,7 +78,7 @@ import junit.framework.Assert;
  * @author Jan De Moerloose
  */
 @RunWith(GwtMockitoTestRunner.class)
-public class WmsServiceTest {
+public class WmsServiceTest extends AbstractWmsServiceTest {
 
 	private static final String VALUE_URL = "http://www.geomajas.org/";
 
@@ -96,52 +98,9 @@ public class WmsServiceTest {
 
 	private WmsUrlTransformer toHelloWorld;
 
-	@Mock
-	RequestBuilder requestBuilder;
-
-	WmsServiceImpl wmsService;
-
-	private String capabilitiesXml;
-
-	private Document capsdoc;
-
 	@Before
 	public void init() throws Exception {
-		wmsService = new WmsServiceImpl();
-		wmsService.setRequestBuilderFactory(new RequestBuilderFactory() {
-
-			@Override
-			public RequestBuilder create(RequestBuilder.Method method, String url) {
-				return requestBuilder;
-			}
-		});
-		wmsService.setCoordinateFormatter(new CoordinateFormatter() {
-
-			@Override
-			public String format(double number) {
-				return new DecimalFormat("#0.0#").format(number);
-			}
-		});
-		wmsService.setUrlEncoder(new UrlEncoder() {
-
-			@Override
-			public String encodeUrl(String url) {
-				return url;
-			}
-		});
-
-		capabilitiesXml = IOUtils.toString(this.getClass().getResourceAsStream("capabilities_1_3_0.xml"), "UTF-8");
-		DOMParser parser = new DOMParser();
-		parser.parse(new InputSource(new StringReader(capabilitiesXml)));
-		capsdoc = w3cToGoogle(parser.getDocument());
-		when(XMLParserImpl.getInstance().parse(anyString())).thenAnswer(new Answer<Document>() {
-
-			@Override
-			public Document answer(InvocationOnMock invocation) throws Throwable {
-				return capsdoc;
-			}
-		});
-		prepareRequestBuilder(requestBuilder, capabilitiesXml, 200);
+		super.init();
 		toHelloWorld = new WmsUrlTransformer() {
 
 			public String transform(WmsRequest request, String url) {
@@ -157,6 +116,7 @@ public class WmsServiceTest {
 
 	@Test
 	public void testGetCapabilities() throws Exception {
+		prepareResponse("capabilities_1_3_0.xml");
 		CaptureCallback callback = new CaptureCallback();
 		wmsService.getCapabilities("http://test", WmsVersion.V1_3_0, callback);
 		WmsGetCapabilitiesInfo info = callback.getResult();
@@ -316,22 +276,6 @@ public class WmsServiceTest {
 
 	}
 
-	private void prepareRequestBuilder(RequestBuilder requestBuilder, String responseText, int statusCode)
-			throws Exception {
-		final Response response = mock(Response.class);
-		when(response.getStatusCode()).thenReturn(statusCode);
-		when(response.getText()).thenReturn(responseText);
-
-		when(requestBuilder.sendRequest(anyString(), any(RequestCallback.class))).thenAnswer(new Answer<Request>() {
-
-			@Override
-			public Request answer(InvocationOnMock invocation) throws Throwable {
-				((RequestCallback) invocation.getArguments()[1]).onResponseReceived(null, response);
-				return null;
-			}
-		});
-	}
-
 	private final class CaptureCallback implements Callback<WmsGetCapabilitiesInfo, String> {
 
 		WmsGetCapabilitiesInfo result;
@@ -354,68 +298,6 @@ public class WmsServiceTest {
 
 		public String getReason() {
 			return reason;
-		}
-
-	}
-
-	private Document w3cToGoogle(final org.w3c.dom.Document document) {
-		return (Document) Proxy.newProxyInstance(getClass().getClassLoader(), new Class<?>[] { Document.class },
-				new W3CToGoogleHandler(document));
-	}
-
-	/**
-	 * Recursive proxy to expose w3c dom classes as gwt dom classes.
-	 * 
-	 * @author Jan De Moerloose
-	 * 
-	 */
-	public class W3CToGoogleHandler implements InvocationHandler {
-
-		private Object delegate;
-
-		public W3CToGoogleHandler(Object delegate) {
-			this.delegate = delegate;
-		}
-
-		@Override
-		public Object invoke(Object target, Method method, Object[] args) throws Throwable {
-			try {
-				Method m = delegate.getClass().getMethod(method.getName(), method.getParameterTypes());
-				Object result = m.invoke(delegate, args == null ? null : new Object[] { args[0] });
-				if (result == null) {
-					return null;
-				}
-				// find the matching interfaces for the return type and create a new proxy for those interfaces
-				Class<?>[] c = getClasses(result, m.getReturnType());
-				if (c.length > 0) {
-					return Proxy.newProxyInstance(getClass().getClassLoader(), c, new W3CToGoogleHandler(result));
-				} else {
-					return result;
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-				throw e;
-			}
-		}
-
-		private Class[] getClasses(Object result, Class type) {
-			List<Class> matches = new ArrayList<Class>();
-			if (result instanceof org.w3c.dom.NamedNodeMap) {
-				matches.add(com.google.gwt.xml.client.NamedNodeMap.class);
-			}
-			if (result instanceof org.w3c.dom.NodeList) {
-				matches.add(com.google.gwt.xml.client.NodeList.class);
-			}
-			if (result instanceof org.w3c.dom.Document) {
-				matches.add(com.google.gwt.xml.client.Document.class);
-			}
-			if (result instanceof org.w3c.dom.Element) {
-				matches.add(com.google.gwt.xml.client.Element.class);
-			}
-			if (result instanceof org.w3c.dom.Node) {
-				matches.add(com.google.gwt.xml.client.Node.class);
-			}
-			return matches.toArray(new Class[0]);
 		}
 
 	}
